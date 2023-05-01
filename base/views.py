03f9c5e35b8f4b47c2913d0369db1f8ef3c2e559
mistrain <mahdimousavi40@gmail.com>
2023-05-01 17:03:14 +0330

    training 1:35:00

--- base/views.py
+++ base/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Developers'},
+]
 
 # Create your views here.
+
+
+def home(request):
+    # return HttpResponse('Home Page')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+
+    context = {'room': room}
+    # return HttpResponse('Room Page')
+    return render(request, 'base/room.html', context)
