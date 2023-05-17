c607d6fbf405021da141627d773046f68d9995c2
mistrain <mahdimousavi40@gmail.com>
2023-05-17 17:53:01 +0330

    02.07.39

--- base/models.py
+++ base/models.py
@@ -17,6 +17,9 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        # dash is for invert
+        ordering = ['-updated', '-created']
     def __str__(self):
         return self.name
 
--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
+
+
 rooms = [
     {'id': 1, 'name': 'Lets learn Python'},
     {'id': 2, 'name': 'Design with me'},
@@ -23,3 +26,36 @@ def room(request, pk):
     context = {'room': room}
     # return HttpResponse('Room Page')
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        print(RoomForm(request.POST))
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    #     print(RoomForm(request.POST))
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    #     print(RoomForm(request.POST))
+    context = {'form': form}
+    return render(request, 'base/delete.html', {'obj': room})
